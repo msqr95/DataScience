652432f5e146325aec0b14016f7bb8265bee1491
Marco Michaelis <82457987+msqr95@users.noreply.github.com>
2025-06-23 08:41:17 +0200

    Update import_pm25.py
    versions

--- import_pm25.py
+++ import_pm25.py
@@ -1,6 +1,6 @@
 # 1. Bibliotheken importieren
-import pandas as pd
-import matplotlib.pyplot as plt
+import pandas as pd #https://pandas.pydata.org/docs/whatsnew/v2.3.0.html
+import matplotlib.pyplot as plt #https://matplotlib.org/stable/users/prev_whats_new/whats_new_3.10.0.html
 
 # 2. Datei laden und nur berliner Stationen filtern
 df = pd.read_csv("pm2_2024-01-01_2025-01-01_all_attributes.csv")
